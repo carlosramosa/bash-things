83c71805b82c75bdefff55c5c4aabd6bcffbf906
carlosramosa <carlosramos.cramos@gmail.com>
2019-04-04 20:35:02 +0200

    feat(init.sh): add brew install commands

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+brew install zsh-syntax-highlighting
+brew install history-substring-search
+brew install zsh-completions
+brew install fortune
+brew install cowsay
+brew install lolcat
+brew install colorls
+
 cp ./zshrc/.zshrc /Users/$USER
 cp ./scripts/.aliasrc /Users/$USER
 cp ./scripts/.autocompleterc /Users/$USER
