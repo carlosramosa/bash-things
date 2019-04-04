14f1e6d8a24fb59e64a815aae648b15bed4ff6dd
carlosramosa <carlosramos.cramos@gmail.com>
2019-04-04 20:30:02 +0200

    feat(init.sh): add init script to copy all files to our user folder

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp ./zshrc/.zshrc /Users/$USER
+cp ./scripts/.aliasrc /Users/$USER
+cp ./scripts/.autocompleterc /Users/$USER
+cp ./scripts/.promptrc /Users/$USER
+cp ./scripts/.stupidrc /Users/$USER
+
